bd310d3435c34b3c2b38a3b013a61f76dd79b2c3
StephenAvila2020 <stephen.r.avila@gmail.com>
2020-05-19 16:07:56 -0400

    added

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -1,10 +1,21 @@
-const apiChatManager = {
-    getUsersFromApi: () => {
-        return fetch(`http://localhost:8088/users`).then((userAvenger) => userAvenger.json()
-        );
-    },
-    getMessagesFromAPi: () => {
-        return fetch(`http://localhost:8088/chat_messages`).then((userMessage) => userMessage.json()
-        );
-    },
+const getMessagesFromApi = () => {
+    fetch(`http://localhost:8088/chat_messages?_expand=user`)
+        .then((dirtyMessages) => dirtyMessages.json()
+        ).then(avengerMessages => {
+            console.log(avengerMessages)
+        });
+}
+
+getMessagesFromApi()
+
+
+const buildUserChat = (singleMessage) => {
+return`<div class="chatbox" id="chat-${avengerMessages}">
+<p>${avengerMessages}</p></div>`
+
+};
+
+const printAllMesages = (messageArray) => {
+    document.querySelector("#chatContainer").innerHTML ="";
+    messageArray.forEach(avengerChat)
 }
