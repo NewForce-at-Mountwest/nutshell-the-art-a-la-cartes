bd989ed594fe14d20035bcce8498cde3a0157f8d
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-05-22 11:52:30 -0400

    login started

--- src/scripts/DOMprinter.js
+++ src/scripts/DOMprinter.js
@@ -1,15 +1,15 @@
-
-
 const everyonePrinter = {
-    loginForm: () => {
-        return `
- <form>
+  loginForm: () => {
+    return `
+    <div id="login">
+      <form>
         <input type="text" id="username-input" placeholder="username">
         <input type="password" id="password-input" placeholder="password">
- </form>
-    <button id="login-btn">Login</button>
-  `
-    }
-}
+      </form>
+      <button id="login-btn">Login</button>
+    </div>
+  `;
+  },
+};
 
-export default everyonePrinter
+export default everyonePrinter;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,57 @@
-import chatEventListeners from "./chat/chatEvent.js"
+import chatEventListeners from "./chat/chatEvent.js";
+import printerObject from "./chat/chatdom.js";
 import chatApiManager from "./chat/chatApi.js";
 import newsEventListener from "./news/newsEventListeners.js";
-import printAll from "./DOMprinter.js"
+import domPrinter from "./domPrinter.js";
 // USER USER USER USER USER USER USER USER USER USER USER USER USER USER USER USER
-printAll.loginForm()
+document.querySelector("#loginContainer").innerHTML += domPrinter.loginForm();
 document.querySelector("body").addEventListener("click", () => {
   if (event.target.id.includes("login-btn")) {
   const usernameValue = document.querySelector("#username-input").value;
   const passwordValue = document.querySelector("#password-input").value;
   console.log(usernameValue, passwordValue);
-  fetch(`http://localhost:8088/users?username=${usernameValue}`)
-    .then((r) => r.json())
-    .then((user) => {
-      console.log(user[0].id);
-      sessionStorage.setItem("userId", user[0].id);
-      // Print all of the restaurants
-      fetchCalls.updateEvent(articleObjectEdit)
-      .then(newsEventListeners.onPageLoadEvent)
-    })
+// PLEASE FOR THE LOVE OF GOD WHY AREN'T YOU WORKING???!?!?!?!?!?
+
+  // fetch(`http://localhost:8088/users?username=${usernameValue}`)
+  //   .then((r) => r.json())
+  //   .then((user) => {
+  //     console.log(user[0].id); })
+      // sessionStorage.setItem("userId", user[0].id);
+      // // Print all of the restaurants
+      // fetchCalls.updateEvent(articleObjectEdit)
+      // .then(newsEventListeners.onPageLoadEvent)
+   
   };
 });
-import printerObject from "./chat/chatdom.js";
+
 // Add a click event listener to the search button
 
-document.querySelector("input.message-form__button").addEventListener("click", chatEventListeners.saveMessageEvent)
+document
+  .querySelector("input.message-form__button")
+  .addEventListener("click", chatEventListeners.saveMessageEvent);
 
 document.querySelector("body").addEventListener("click", () => {
-    if (event.target.id.includes("delete-chat")) {
-        chatEventListeners.deleteMessageEvent();
-    } else if (event.target.id.includes("edit-chat")) {
-        chatEventListeners.printEditForm();
-    } else if (event.target.id.includes("save-changes")) {
-        chatEventListeners.saveMessageChangesEvent();
-    }
-})
+  if (event.target.id.includes("delete-chat")) {
+    chatEventListeners.deleteMessageEvent();
+  } else if (event.target.id.includes("edit-chat")) {
+    chatEventListeners.printEditForm();
+  } else if (event.target.id.includes("save-changes")) {
+    chatEventListeners.saveMessageChangesEvent();
+  }
+});
 
-chatEventListeners.onPageload()
-
+chatEventListeners.onPageload();
 
 newsEventListener.onPageLoadEvent();
 // Save event
 document.querySelector("body").addEventListener("click", function () {
   if (event.target.id.includes("save-btn")) {
-    newsEventListener.saveCardEvent()
+    newsEventListener.saveCardEvent();
   } else if (event.target.id.includes("delete-btn-")) {
-    newsEventListener.deleteEvent()
+    newsEventListener.deleteEvent();
   } else if (event.target.id.includes("edit-btn")) {
-    newsEventListener.editEvent1()
+    newsEventListener.editEvent1();
   } else if (event.target.id.includes("saveEdit-btn")) {
-   newsEventListener.editEventSave()
+    newsEventListener.editEventSave();
   }
 });
